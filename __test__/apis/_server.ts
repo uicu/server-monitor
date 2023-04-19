2361bcf51a62bd7d11eb705ee7a97ed15749d812
yangpan <738912168@qq..com>
2023-04-19 12:20:04 +0800

    ci(actions): 增加流水线

--- __test__/apis/_server.ts
+++ __test__/apis/_server.ts
@@ -21,7 +21,7 @@ if (isTestLocal) {
 // 存储登录 token ，统一拼接 headers.Authorization
 let TOKEN = '';
 // 测试机 host
-const REMOTE_HOST = 'http://43.129.75.108:8081'; // TODO 待定
+const REMOTE_HOST = 'http://43.129.75.108:8080'; // TODO 待定
 //发送请求
 async function ajax(
 	method: string,
